ed6e1fcc7b0c834ff02dac83f19184a94db71f68
Solomon Moyo <solomon.moyo@dariel.co.za>
2025-07-18 01:13:17 +0200

    maintenance update

--- src/app/components/services/services.ts
+++ src/app/components/services/services.ts
@@ -11,41 +11,6 @@ export class Services {
   selectedServiceImages: string[] = [];
   selectedImageIndex: number = 0;
   currentYear: number = new Date().getFullYear();
-  // services = [
-  //   {
-  //     title: 'Aluminium Fabrication',
-  //     description:
-  //       'Doors, windows, balustrades, and shopfronts — built to endure and impress.',
-  //     images: [
-  //       'https://5.imimg.com/data5/SELLER/Default/2021/12/OP/OF/QR/49521759/aluminium-door-fabrication-works-500x500-500x500.jpg',
-  //       'https://tse2.mm.bing.net/th/id/OIP.u4DPLI7ZmnYwX_R_rjXaMgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3',
-  //       'https://3.imimg.com/data3/IR/KH/MY-3018077/aluminum-door-500x500.jpg',
-  //       'https://tse1.mm.bing.net/th/id/OIP.kJqqySO2pv8L_9FvJoInBwHaFj?rs=1&pid=ImgDetMain&o=7&rm=3',
-  //       'https://s3.ap-northeast-2.wasabisys.com/canterburybalustrade/2022/09/Canterbury-Balustrade-Aluminium-Framed-Windsor-with-motif-773x1030.jpg',
-  //       'https://prestigealuminium.co.uk/wp-content/uploads/2021/12/Aluminium-Shopfronts-Nottingham.jpg',
-  //     ],
-  //   },
-  //   {
-  //     title: 'Welding Solutions',
-  //     description:
-  //       'From gate work to custom frames — we weld strength into every structure.',
-  //     images: [
-  //       'https://i.ytimg.com/vi/HHQ47gHIsdc/maxresdefault.jpg',
-  //       'https://www.yogiemachinery.com/Content/uploads/2023205814/20230505114255f6fb1e392f314cdf96d0c88593538d5e.jpg',
-  //       'https://tse1.explicit.bing.net/th/id/OIP.yG_VPrhpp-yMHlw6mfsLRgHaMn?rs=1&pid=ImgDetMain&o=7&rm=3',
-  //     ],
-  //   },
-  //   {
-  //     title: 'Repairs & Installations',
-  //     description:
-  //       'Onsite mobile repairs and seamless installations for home or business projects.',
-  //     images: [
-  //       'https://as1.ftcdn.net/v2/jpg/00/04/04/14/1000_F_4041414_TSuYJMSPFXdHc8ADwOQYOjdmxsAarFXA.jpg',
-  //       'https://tse1.mm.bing.net/th/id/OIP.5XqW29fAUc6-j6h8qNu80wHaFj?rs=1&pid=ImgDetMain&o=7&rm=3',
-  //       'https://tse4.mm.bing.net/th/id/OIP.kJKd1WYwS_r7H03ruxwesAHaHa?rs=1&pid=ImgDetMain&o=7&rm=3',
-  //     ],
-  //   },
-  // ];
 
   services = [
     {
@@ -115,6 +80,13 @@ export class Services {
   openImage(images: string[], index: number): void {
     this.selectedServiceImages = images;
     this.selectedImageIndex = index;
+    this.preloadImages(images);
+  }
+  preloadImages(images: string[]) {
+    images.forEach((imgUrl) => {
+      const img = new Image();
+      img.src = imgUrl;
+    });
   }
 
   closeImage(): void {
